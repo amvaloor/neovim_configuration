df40c23b4915a79d1430c0e7e672e903ee9e5abe
Akshay <avaloor@umich.edu>
2026-02-01 01:15:04 -0500

    Plugin: Neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,38 @@
+return {
+    -- Neo-tree
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", 
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("neo-tree").setup({
+                filesystem = {
+                    filtered_items = {
+                        visible = true,             -- master switch to show hidden files/dotfiles
+                        hide_dotfiles = false,      -- makes neo-tree show files that start with a dot
+                        hide_gitignored = false,    -- makes neo-tree show files that are in the .gitignore
+                    },
+                },
+            })
+          
+          -- Keymap to toggle the sidebar
+          vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+        end
+      },
+    
+    -- Auto-update imports when renaming files
+    {
+        "antosha417/nvim-lsp-file-operations",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-neo-tree/neo-tree.nvim",
+        },
+        config = function()
+            require("lsp-file-operations").setup()
+        end,
+    },
+}
