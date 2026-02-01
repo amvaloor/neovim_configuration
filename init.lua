aedc5943373854054e8229031585517181b6fbce
Akshay <avaloor@umich.edu>
2026-01-31 20:10:22 -0500

    Initial commit

---
+++ init.lua
@@ -0,0 +1,22 @@
+-- Enable line numbers
+vim.opt.number = true
+
+-- Show status line w/ file
+vim.opt.laststatus = 3
+
+-- Substitute tabs with four spaces
+vim.opt.expandtab = true    -- typing tabs replaces them with spaces
+vim.opt.tabstop = 4         -- nvim displays \t characters as four spaces long
+vim.opt.shiftwidth = 4      -- auto-indents are four spaces wide
+vim.opt.softtabstop = 4     -- the tab and backspace characters affect 4 spaces
+
+-- Keep tabs in Makefiles
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "mk", "make" },
+    callback = function()
+        vim.opt_local.expandtab = false
+    end,
+})
+
+-- Set up package manager
+require("config.lazy")
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,43 @@
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        branch = "master",
+        build = ':TSUpdate',
+
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                -- Parsers to have installed by default
+                ensure_installed = {
+                    "lua",
+                    "html",
+                    "css",
+                    "vim",
+                    "vimdoc",
+                    "c",
+                    "cpp",
+                    "rust",
+                    "go"
+                },
+
+                -- Parsers can download in the background while a file is being edited
+                sync_install = false,
+
+                -- If a filetype is opened without a parser, its parser will auto-install
+                auto_install = true,
+
+                -- Disable default syntax highlighting and activate Treesitter
+                highlight = {
+                    enable = true, 
+                    additional_vim_regex_highlighting = false,
+                },
+
+                -- Use Treesitter's auto-indentation
+                indent = { 
+                    enable = true 
+                },
+            })
+        end
+    }
+}
