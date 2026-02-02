a824499cb8dd8eab547ea704660a2019e44a363a
Akshay <avaloor@umich.edu>
2026-02-02 18:50:11 -0500

    feat: lazydev to blink communication

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -2,6 +2,9 @@
 return {
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            "folke/lazydev.nvim",
+        },
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {}),
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {}),
         vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {}),
