73dd70e7fee16737478fcc09b90ea62120a9d081
Akshay <avaloor@umich.edu>
2026-02-02 17:17:29 -0500

    plugin: autopairs

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -11,7 +11,10 @@ return {
                 nerd_font_variant = 'mono'
             },
 
-            completion = { documentation = { auto_show = false } },
+            completion = {
+                documentation = { auto_show = false },
+                accept = { auto_brackets = { enabled = true } },
+            },
 
             sources = {
                 default = { 'lsp', 'path', 'snippets', 'buffer' },
---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "leoluz/nvim-dap-go",
+        },
+    },
+}
