8da46e28cf972e4c45e6d15e130b04483508efd2
Akshay <avaloor@umich.edu>
2026-02-03 14:04:48 -0500

    keymaps: dap and dap-ui

--- lua/plugins/debugging/dap.lua
+++ lua/plugins/debugging/dap.lua
@@ -3,11 +3,11 @@ return {
         "mfussenegger/nvim-dap",
         config = function()
             local dap = require("dap")
-            vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc =  "Toggle Breakpoint" })
-            vim.keymap.set("n", "<leader>d<Left>", dap.restart_frame, { desc = "Restart Frame" })
-            vim.keymap.set("n", "<leader>d<Right>", dap.step_over, { desc = "Step Over" })
-            vim.keymap.set("n", "<leader>d<Up>", dap.step_out, { desc = "Step Out" })
-            vim.keymap.set("n", "<leader>d<Down>", dap.step_into, { desc = "Step Into" })
+            vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc =  "Debug: Toggle Breakpoint" })
+            vim.keymap.set("n", "<leader>d<Left>", dap.restart_frame, { desc = "Debug: Restart Frame" })
+            vim.keymap.set("n", "<leader>d<Right>", dap.step_over, { desc = "Debug: Step Over" })
+            vim.keymap.set("n", "<leader>d<Up>", dap.step_out, { desc = "Debug: Step Out" })
+            vim.keymap.set("n", "<leader>d<Down>", dap.step_into, { desc = "Debug: Step Into" })
         end
     },
 }
