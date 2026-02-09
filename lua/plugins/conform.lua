8af0c9ef291de88c11e33aa70deb3f786c540ef5
Akshay <avaloor@umich.edu>
2026-02-09 02:51:54 -0500

    plugin: conform.nvim

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        'stevearc/conform.nvim',
+        config = function()
+            require("conform").setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    python = { "isort", "ruff_format" },
+                    rust = { "rustfmt" },
+                },
+                format_on_save = {
+                    timeout_ms = 500,
+                    lsp_format = "fallback",
+                },
+            })
+        end
+    }
+}
