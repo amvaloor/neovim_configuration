2855c3b3fd0e6d466333f45cf876d041eaf904f7
Akshay Valoor <avaloor@umich.edu>
2026-05-06 01:12:35 -0400

    plugin: gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,18 @@
+return {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+        signs      = {
+            add          = { text = '┃' },
+            change       = { text = '┃' },
+            delete       = { text = '_' },
+            topdelete    = { text = '‾' },
+            changedelete = { text = '~' },
+            untracked    = { text = '┆' },
+        },
+        -- This makes the signs look like a solid bar (VSCode style)
+        signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+        numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    },
+}
